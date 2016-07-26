9e4dbc5c9aa22a385aba27edfb7c45e746a89a94
Anirudh Vemula <vvanirudh@gmail.com>
2016-07-26 15:36:47 -0400

    Created a new branch for twoagents setting

--- Cooperative/twoagents_DPGP.m
+++ Cooperative/twoagents_DPGP.m
@@ -5,8 +5,8 @@
 % by Anirudh Vemula, Jul 18, 2016
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear all;
-close all;
+clearvars -except trajs
+%close all;
 clc;
 
 % adding directory path
@@ -20,6 +20,10 @@ addpath genTraj
 addpath ../external
 addpath twoagents_plotting
 
+% testing variables
+indep_test = true;
+reinitialize_trajs = false;
+
 % global variables
 global trajs sparseGPs
 
@@ -28,7 +32,7 @@ lx = 2;
 ly = 2;
 
 %% generate n trajectories for both agents
-n_traj = 7;
+n_traj = 10;
 n_points = 30;
 x_min = -5; x_max = 5; y_min = -5; y_max = 5;
 pLimit = [x_min, x_max, y_min, y_max];
@@ -36,14 +40,18 @@ speed = 1.0;
 sigma_noise = 0.05;
 
 % Implement generateTwoAgentTrajs
-trajs = generateTwoAgentTrajs(n_traj, n_points, pLimit, speed, ...
-                              sigma_noise);
+if reinitialize_trajs
+    trajs = generateTwoAgentTrajs(n_traj, n_points, pLimit, speed, ...
+                                  sigma_noise);
+end
 
 n_traj = trajs.n_traj;
-%plotTrajs(trajs, 'initialization');
+if reinitialize_trajs
+    twoagents_plotTrajs(trajs, 'initialization');
+end
 
 %% Two Agent DPGP
-sigma_noise = 1.0;
+sigma_noise = 0.5;
 sigma_input = 1;
 hyperparam = [lx, ly, sigma_input, sigma_noise];
 n_sweep = 200;
@@ -83,66 +91,74 @@ for sweep_num=1:n_sweep
     L = zeros(trajs.n_traj, trajs.n_clus+1, trajs.n_clus+1);
     L_GP = zeros(trajs.n_traj, trajs.n_clus+1, trajs.n_clus+1);
     rand_ordering = 1:trajs.n_traj; %randperm(trajs.n_traj)
+
+    L_1 = zeros(trajs.n_traj, trajs.n_clus);
+    L_GP_1 = zeros(trajs.n_traj, trajs.n_clus);
+    
+    L_2 = zeros(trajs.n_traj, trajs.n_clus);
+    L_GP_2 = zeros(trajs.n_traj, trajs.n_clus);
+    
     
     for kk=1:trajs.n_traj
         k = rand_ordering(kk);
         
-        % p(t1, t2) = p(t1)*p(t2|t1)
-        % Initially, likelihood p(t|b)
-        for j1=1:trajs.n_clus
+        for j=1:trajs.n_clus
             trajs.data1(k).DP_alpha = alpha;
             
-            if j1 == trajs.cluster(k, 1, sweep_num)
-                n_k_1 = count(j1) - 1;
+            if j== trajs.cluster(k, 1, sweep_num)
+                n_k = count(j) - 1;
             else
-                n_k_1 = count(j1);
+                n_k = count(j);
             end
             
-            if(n_k_1 == 0)
-                n_k_1 = alpha;
+            if(n_k == 0)
+                n_k = alpha;
             end
             
-            % Calculate the likelihood of p(t1|b_j1)
-            likelihood_1_GP = ...
-                DP_traj_likelihood_indep(sparseGPs(j1).sparseGP_x, ...
-                                         sparseGPs(j1).sparseGP_y, ...
+            L_GP_1(k, j) = ...
+                DP_traj_likelihood_indep(sparseGPs(j).sparseGP_x, ...
+                                         sparseGPs(j).sparseGP_y, ...
                                          trajs.data1(k));
-            likelihood_1 = likelihood_1_GP + log (n_k_1 / ...
-                                                  (trajs.n_traj*2 ...
-                                                   - 1 + alpha));
+            L_1(k, j) = L_GP_1(k,j) + log (n_k / (trajs.n_traj*2 - ...
+                                                  1 + alpha));
             
-            for j2=1:trajs.n_clus                
-                trajs.data2(k).DP_alpha = alpha;
-                
-                if j2 == trajs.cluster(k, 2, sweep_num)
-                    n_k_2 = count(j2) - 1;
-                else
-                    n_k_2 = count(j2);
-                end                                
-                
-                if(n_k_2 == 0)
-                    n_k_2 = alpha;
-                end
-                
-                % Now to calculate the likelihood of p(t2|b_j1, t1,
-                % b_j2)
-                likelihood_2_GP = ...
-                    DP_traj_likelihood_dep(sparseGPs(j2).sparseGP_x, ...
-                                           sparseGPs(j2).sparseGP_y, ...
-                                           trajs.data2(k), ...
-                                           trajs.data1(k));                
-                
-                likelihood_2 = likelihood_2_GP + log(n_k_2 / ...
-                                                     (trajs.n_traj*2 ...
-                                                      - 1 + ...
-                                                      alpha));
-                
-                L_GP(k, j1, j2) = likelihood_1_GP + ...
-                    likelihood_2_GP;
-                L(k, j1, j2) = likelihood_1 + likelihood_2;                    
+            trajs.data2(k).DP_alpha = alpha;
+            
+            if j == trajs.cluster(k, 2, sweep_num)
+                n_k = count(j) - 1;
+            else
+                n_k = count(j);
             end
+            
+            if(n_k == 0)
+                n_k = alpha;
+            end
+            
+            if ~indep_test
+                L_GP_2(k, j) = ...
+                    DP_traj_likelihood_dep(sparseGPs(j).sparseGP_x, ...
+                                           sparseGPs(j).sparseGP_y, ...
+                                           trajs.data2(k), ...
+                                           trajs.data1(k));                    
+            else
+                L_GP_2(k, j) = ...
+                    DP_traj_likelihood_indep(sparseGPs(j).sparseGP_x, ...
+                                             sparseGPs(j).sparseGP_y, ...
+                                             trajs.data2(k));
+                
+            end
+            L_2(k, j) = L_GP_2(k,j) + log (n_k / (trajs.n_traj*2 - ...
+                                                  1 + alpha));
         end
         
+        L_GP(k, 1:end-1, 1:end-1) = repmat(L_GP_1(k, :)', 1, ...
+                                           trajs.n_clus) + ...
+            repmat(L_GP_2(k, :), trajs.n_clus, 1);
+        
+        L(k, 1:end-1, 1:end-1) = repmat(L_1(k, :)', 1, ...
+                                           trajs.n_clus) + ...
+            repmat(L_2(k, :), trajs.n_clus, 1);
+                   
         % DOUBT
         normalization_const = max(max(L(k, 1:end-1, 1:end-1)));
         l = squeeze(L(k,:,:)) - normalization_const;
@@ -221,7 +237,7 @@ splicing = 2;
 trajs.cluster(:, 1, end) = mode1';
 trajs.cluster(:, 2, end) = mode2';
 
-twoagents_plotTrajs(trajs, 'mode');
+%twoagents_plotTrajs(trajs, 'mode');
 
 %trajs.cluster(:, 1, end) = avgSample1';
 %trajs.cluster(:, 2, end) = avgSample2';
@@ -232,10 +248,12 @@ twoagents_groupTraj(sweep_num);
 twoagents_build_SparseGPs_array(hyperparam);
 for i=1:trajs.n_traj
     [ind1, ind2] = twoagents_findBestPattern(trajs.data1(i), ...
-                                          trajs.data2(i));
+                                          trajs.data2(i), indep_test);
     trajs.cluster(i, 1, end) = ind1;
     trajs.cluster(i, 2, end) = ind2;
 end
+twoagents_groupTraj(sweep_num);
+twoagents_build_SparseGPs_array(hyperparam);
 twoagents_plotTrajs(trajs, 'reassigned cluster');
 
 
